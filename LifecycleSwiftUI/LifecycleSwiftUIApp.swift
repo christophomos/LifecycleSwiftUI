074dee12d37f55766a636c67af6fba0a44f95e53
Chris Clark <chris@qalang.com>
2020-10-05 22:11:39 +0800

    Initial Commit

---
+++ LifecycleSwiftUI/LifecycleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifecycleSwiftUIApp.swift
+//  LifecycleSwiftUI
+//
+//  Created by Chris Clark on 2020/10/5.
+//
+
+import SwiftUI
+
+@main
+struct LifecycleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
